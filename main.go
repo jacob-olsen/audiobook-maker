6256cba27e565087de3c4dccd3066459c610efbc
jacob olsen <jacobspil@hotmail.dk>
2024-11-27 23:21:15 +0100

    start work on web

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	metahandler "audiobook-maker/metaHandler"
+	"net/http"
 )
 
 func main() {
 	metahandler.MakeTabels()
+}
+
+func LoadUrlPaths() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+	})
 }
